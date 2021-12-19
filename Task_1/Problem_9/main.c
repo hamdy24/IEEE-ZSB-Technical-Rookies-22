cffd0355fe4345c57f6f239e3a11448a6a726580
hamdy24 <hamdyaboaouf24@gmail.com>
2021-12-19 12:55:26 +0200

    Generating fibonacci numbers till a specific number

--- Task_1/Problem_9/main.c
+++ Task_1/Problem_9/main.c
@@ -19,24 +19,3 @@ int main()
 
     return 0;
 }
-
-/*
-void main()
-{
-    // generate fibonacci
-    int n;
-    int previous = 1;
-    int number = 0;
-
-    scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++)
-    {
-        printf("%d\t", previous);
-        previous += number;
-        number = previous - number;
-    }
-}
-
-*/
-
